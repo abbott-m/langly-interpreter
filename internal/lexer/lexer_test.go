114a209ec138d3b1ab137e00d83e40d8e3b231ed
Michael Abbott <michael@mabbott.dev>
2024-04-04 15:29:33 +0100

    add numeric support and identifiers

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -31,6 +31,57 @@ func TestNextToken(t *testing.T) {
 				{token.EOF, ""},
 			},
 		},
+		{
+			input: `
+				let five = 5;
+				let ten = 10;
+
+				let add = fn(x, y) {
+				  x + y;
+				};
+
+				let result = add(five, ten);
+			`,
+			expected: []Expected{
+				{token.LET, "let"},
+				{token.IDENT, "five"},
+				{token.ASSIGN, "="},
+				{token.INT, "5"},
+				{token.SEMICOLON, ";"},
+				{token.LET, "let"},
+				{token.IDENT, "ten"},
+				{token.ASSIGN, "="},
+				{token.INT, "10"},
+				{token.SEMICOLON, ";"},
+				{token.LET, "let"},
+				{token.IDENT, "add"},
+				{token.ASSIGN, "="},
+				{token.FUNCTION, "fn"},
+				{token.LPAREN, "("},
+				{token.IDENT, "x"},
+				{token.COMMA, ","},
+				{token.IDENT, "y"},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.IDENT, "x"},
+				{token.PLUS, "+"},
+				{token.IDENT, "y"},
+				{token.SEMICOLON, ";"},
+				{token.RBRACE, "}"},
+				{token.SEMICOLON, ";"},
+				{token.LET, "let"},
+				{token.IDENT, "result"},
+				{token.ASSIGN, "="},
+				{token.IDENT, "add"},
+				{token.LPAREN, "("},
+				{token.IDENT, "five"},
+				{token.COMMA, ","},
+				{token.IDENT, "ten"},
+				{token.RPAREN, ")"},
+				{token.SEMICOLON, ";"},
+				{token.EOF, ""},
+			},
+		},
 	}
 
 	for i, tt := range tests {
